291c0a46c5fa0082d3d0983bc55c1281a5d42181
Widr <49525595+wwwidr@users.noreply.github.com>
2026-04-03 10:17:50 -0700

    Add CompanyFactory

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Modules\Company\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    protected $model = Company::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'status' => 'active',
+        ];
+    }
+
+    public function inactive(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'status' => 'inactive',
+        ]);
+    }
+}
